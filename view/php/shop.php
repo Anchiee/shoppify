6cc5ddcc599cffc6d61fbc9fb249aa8cccf8198d
Anchie <wyhwtf@gmail.com>
2024-12-05 16:35:07 +0100

    Added a split between search section and suggested products

--- view/php/shop.php
+++ view/php/shop.php
@@ -49,33 +49,39 @@
     </form>
 
 
-    <div class="container">
+    <div class="products-container">
+      <p class="suggested-header">Suggested products</p>
+
+      <div class="container">
+
+        <?php
+          require "../../model/database/ReadProducts.php";
+
+          $result = returnAllProducts();
+          
+          for($i = 0; $i < 3; $i++)
+          {
+            $randomProductIndex = rand(0, sizeof($result) - 1);
+
+            echo "<form action='../../controller/FormHandlerCart.php' method='post' class='product-container'>"; 
+            echo "<p>ID:" . $result[$randomProductIndex]["id"] . "</p>";
+            echo "<h1>" . $result[$randomProductIndex]["productName"] . "</h1>";
+            echo "<input type='hidden' name='product-name' value='" . $result[$randomProductIndex]["productName"] . "'>";
+            echo "<p>Price:" . $result[$randomProductIndex]["price"] . "</p>";
+            echo "<h2>Processor speed</h2>";
+            echo "<p>Base clock speed:" . $result[$randomProductIndex]["base_clock"] . "</p>";
+            echo "<p>Boosted clock speed:" . $result[$randomProductIndex]["boosted_clock"] . "</p>";
+            echo "<p>ROMStorage:" . $result[$randomProductIndex]["ROMStorage"] . "</p>";
+            echo "<p>RAMStorage:" . $result[$randomProductIndex]["RAMStorage"] . "</p>";
+            echo "<button class='check-out'>Add to cart</button>";
+            echo "</form>";
+          }
+
+          
+        ?>
+      </div>
+
       
-      <?php
-        require "../../model/database/ReadProducts.php";
-
-        $result = returnAllProducts();
-        
-        for($i = 0; $i < 3; $i++)
-        {
-          $randomProductIndex = rand(0, sizeof($result) - 1);
-
-          echo "<form action='../../controller/FormHandlerCart.php' method='post' class='product-container'>"; 
-          echo "<p>ID:" . $result[$randomProductIndex]["id"] . "</p>";
-          echo "<h1>" . $result[$randomProductIndex]["productName"] . "</h1>";
-          echo "<input type='hidden' name='product-name' value='" . $result[$randomProductIndex]["productName"] . "'>";
-          echo "<p>Price:" . $result[$randomProductIndex]["price"] . "</p>";
-          echo "<h2>Processor speed</h2>";
-          echo "<p>Base clock speed:" . $result[$randomProductIndex]["base_clock"] . "</p>";
-          echo "<p>Boosted clock speed:" . $result[$randomProductIndex]["boosted_clock"] . "</p>";
-          echo "<p>ROMStorage:" . $result[$randomProductIndex]["ROMStorage"] . "</p>";
-          echo "<p>RAMStorage:" . $result[$randomProductIndex]["RAMStorage"] . "</p>";
-          echo "<button class='check-out'>Add to cart</button>";
-          echo "</form>";
-        }
-
-        
-      ?>
     </div>
 
 
