0c5ba826f113346d6b249b84a19439650ae1373a
Anchie <wyhwtf@gmail.com>
2024-12-12 14:53:16 +0100

    Changed the image path to absolute

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <title>Main menu</title>
     <link rel="stylesheet" href="./view/style/index.css">
     <link rel="stylesheet" href="./view/style/header.css">
@@ -10,7 +10,22 @@
     <meta charset="UTF-8">
     <meta property="og:title" content="Shoppify">
     <meta property="og:description" content="Shopping website. Shop whenever and whatever you want!">
-    <meta property="og:image" content="./assets/logo.png">
+    <meta property="og:image" content="http://shoppify.infinityfreeapp.com/assets/logo.png">
+    <meta property="og:type" content="website">
+    <meta property="og:logo" content="http://shoppify.infinityfreeapp.com/assets/logo.png">
+    <meta property="og:url" content="http://shoppify.infinityfreeapp.com/">
+    <meta name="description" content="Shoppify is a website where you can buy several products. Starting from the cheapest to the most expensive ones. It offers the highest quality">
+    <meta name="keywords" content="shop, buy, products">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="http://shoppify.infinityfreeapp.com/">
+    <meta property="og:locale" content="en_US">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Shoppify">
+    <meta name="twitter:description" content="Shoppify. Shop wherever you want and whenever you want.">
+    <meta name="author" content="Anchie">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta property="fb:app_id" content="934771528126307">
+    <meta property="og:site_name" content="Shoppify">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
