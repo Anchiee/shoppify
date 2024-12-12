1f72a19725c60bad82456a930bf4e75b610fe9f0
Anchie <wyhwtf@gmail.com>
2024-12-12 14:54:39 +0100

    Changed the image path to the absolute path

--- view/php/templates/head.php
+++ view/php/templates/head.php
@@ -3,10 +3,24 @@
 <meta charset="UTF-8">
 <meta property="og:title" content="Shoppify">
 <meta property="og:description" content="Shopping website. Shop whenever and whatever you want!">
-<meta property="og:image" content="../../assets/logo.png">
+<meta property="og:image" content="http://shoppify.infinityfreeapp.com/assets/logo.png">
+<meta property="og:type" content="website">
+<meta property="og:logo" content="http://shoppify.infinityfreeapp.com/assets/logo.png">
+<meta property="og:url" content="http://shoppify.infinityfreeapp.com/">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="icon" href="../../assets/logo.png">
-
+<meta name="description" content="Shoppify is a website where you can buy several products. Starting from the cheapest to the most expensive ones. It offers the highest quality">
+<meta name="keywords" content="shop, buy, products">
+<meta name="robots" content="index, follow">
+<link rel="canonical" href="http://shoppify.infinityfreeapp.com/">
+<meta property="og:locale" content="en_US">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="Shoppify">
+<meta name="twitter:description" content="Shoppify. Shop wherever you want and whenever you want.">
+<meta name="author" content="Anchie">
+<meta property="fb:app_id" content="934771528126307">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:site_name" content="Shoppify">
